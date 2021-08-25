f9db088f36b3470e9643e148db29b7a5c2ab4b75
Nicolas <Nicolas.Neira@Kairosmining.com>
2021-08-25 01:28:07 -0400

    adding 2 new charts, still need to organize the code

--- main.py
+++ main.py
@@ -32,8 +32,26 @@ def get_csv_data(raw_path):
   return raw_path[:-4].split("/")[2][:10] , pd.read_csv(url_path, index_col=0)
 
 
+# Descomprime la lista entera de datasets historicos y les appendea la columna Fecha como indice
+def extract_csv_data(product_dataset_list : list):
+  # Getting datasets from each link
+  data_list = pd.DataFrame
+  i = 0
+
+  for link in product_dataset_list:
+    date, data = get_csv_data(link)
+    #data['Fecha'] = date
+    data.insert(0, "Fecha", date)
+    if (i == 0):
+      data_list = data
+    else:
+      data_list = data_list.append(data)
+    i = i + 1
+  
+  return data_list
+
+
 producto5_csvs = get_list_csv(5)
-data_list_p5 = pd.DataFrame
 _, df_producto5  = get_csv_data(producto5_csvs[1])
 df_producto5.index = pd.to_datetime(df_producto5.index)
 
@@ -88,6 +106,35 @@ col2.metric("Casos nuevos actuales ", locale.format('%d',  current, grouping=Tru
 col3.metric("Delta respecto últ. 15 días ", str(delta)[0:3]+"%")
 st.line_chart(df_producto5['Casos nuevos totales'])
 
+
+### DATASETS INTERESANTES:
+# DP46 Curva activos vs recuperados -> https://github.com/MinCiencia/Datos-COVID19/blob/master/output/producto46/activos_vs_recuperados.csv
+st.title("Curva activos vs recuperados")
+p46 = get_list_csv(46)
+_, df  = get_csv_data(p46[0])
+df.index = pd.to_datetime(df.index)
+
+st.line_chart(df)
+
+# DP1 Totales por comuna incremental -> https://github.com/MinCiencia/Datos-COVID19/blob/master/output/producto1/Covid-19.csv
+
+st.title("Casos confirmados acumulados por comuna de Santiago")
+p2 = get_list_csv(2)
+_, df  = get_csv_data(p2[len(p2)-1])
+#import ipdb; ipdb.set_trace()
+lista_comunas = ['Puente Alto', 'La Florida', 'Melipilla', 'Las Condes', 'Providencia','Maipu']
+df_p = df.loc[df.Comuna.isin(lista_comunas)]
+df_p = df_p.tail(len(lista_comunas))
+df_p = df_p.reset_index().set_index('Comuna')
+df_p = df_p.sort_values('Poblacion')
+
+chart_data = pd.concat([df_p['Casos Confirmados'], df_p['Poblacion']], axis=1)
+
+st.bar_chart(chart_data)
+
+# DP4 - Casos totales por región: Descripción -> https://github.com/MinCiencia/Datos-COVID19/tree/master/output/producto4
+# DP76 - Avance en Campaña de Vacunación COVID-19 -> https://github.com/MinCiencia/Datos-COVID19/blob/master/output/producto76/vacunacion_t.csv | https://github.com/MinCiencia/Datos-COVID19/blob/master/output/producto76/grupo_t.csv | https://github.com/MinCiencia/Datos-COVID19/blob/master/output/producto76/fabricante_t.csv
+# ISCI Producto movilidad por comunas ->  https://github.com/MinCiencia/Datos-COVID19/tree/master/output/producto82
 
 
 
